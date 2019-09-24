d2897a9db6ab1fa8decb7b838c940b57ae5c5321
katsuhah <hideki.katsuhara@justsystems.com>
2019-09-24 14:46:58 +0900

    SSpecspec3:  実行確認版

--- src/DictionaryReader.java
+++ src/DictionaryReader.java
@@ -1,3 +1,4 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -6,17 +7,19 @@ import java.io.InputStreamReader;
 
 public class DictionaryReader {
 
+	private static final String DATA_PATH = "/Users/katsuhah/project/_temp/20190924_natori/";
 	private static final String DIC_DATA_PATH = "data/dictionary-data.txt";
 
 	public DictionaryReader() {
 	}
-	
+
 	public void out() {
 		// TODO ファイルから辞書を読み取る
-		try (InputStream is = new FileInputStream(DIC_DATA_PATH);
-				InputStreamReader iSReader = new InputStreamReader(is, "UTF-8")) {
-			int data;
-			while ((data = iSReader.read()) != -1) {
+		try (InputStream is = new FileInputStream(DATA_PATH + DIC_DATA_PATH);
+				InputStreamReader iSReader = new InputStreamReader(is, "UTF-8");
+					BufferedReader bReader = new BufferedReader(iSReader) ) {
+			String data;
+			while ((data = bReader.readLine()) != null) {
 				System.out.println(data);
 			}
 		} catch (FileNotFoundException e) {
