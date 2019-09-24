f9ce65786ec30778c0d0831e7029b26e0bd0ccd9
katsuhah <hideki.katsuhara@justsystems.com>
2019-09-24 15:10:21 +0900

    Spec4:  データファイルの格納数を拡張 （コメント修正）

--- src/Exec.java
+++ src/Exec.java
@@ -1,10 +1,10 @@
 public class Exec {
 
+	// データファイルの単語を読み込む場合は、第一引数にデータファイルのプルパスを指定する
 	public static void main(String[] args) {
 		if (args.length == 0) {
 			System.out.println("上手");
 		}
-		// 第一引数はデータファイルのフルパス
 		new DictionaryReader(args[0]).out();
 	}
 }
